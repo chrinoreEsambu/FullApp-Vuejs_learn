4024063bfb25310142b60ae699b385572967d8b0
chrinore <chrinoren@gmail.com>
2025-06-21 09:13:48 +0100

    premier commit

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,21 +1,31 @@
+import axios from 'frontend/node_modules/axios/dist/axios.js';
 <template>
-  <div>
-    <div class="text-2xl font-bold text-blue-600 flex flex-col">
-      Bonjour Vue + Tailwind !
-      <input type="bg" class="border-black rounded-[5px]" />
-      <!-- <router-link :to="{ name: 'register' }">go to register</router-link> -->
-      <label @click="register">go to page</label>
+  <div class="border-box">
+    <div class="flex items-center flex-col mt-[5em] p-11">
+      <input
+        type="text"
+        v-model="user_id"
+        class="mb-3 p-2 w-[50vw] border-[1px] border-gray-950 rounded[2px]"
+      />
+      <input
+        type="password"
+        v-model="password"
+        class="mb-3 p-2 w-[50vw] border-[1px] border-gray-950 rounded[2px]"
+      />
+      <button
+        @click="submitlog"
+        class="bg-black text-white p-2 w-[50vw] rounded[2px]"
+      >
+        send
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
-const router = useRouter();
+import { ref } from "vue";
+const user_id = ref("");
 
-const register = () => {
-  router.push("/register");
-};
 </script>
 
 <style lang="scss" scoped></style>
